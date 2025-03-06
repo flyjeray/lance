42b92183793828226589678b8b98bc69ae66779a
flyjeray <flyjeray@gmail.com>
2025-03-06 22:10:59 +0300

    chore(backend): use APIResponse at AuthMiddleware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -3,6 +3,7 @@ import { UserModel } from '@lance/shared/models/user';
 import { Request, Response, NextFunction } from 'express';
 import { config } from '@/config';
 import { body, validationResult } from 'express-validator';
+import { APIResponse } from '@lance/shared/models/api/general';
 
 export type VerifiedUserRequest = Request & {
   user: string;
@@ -12,10 +13,10 @@ class AuthMiddleware {
   static validateLoginCredentials = [
     body('login').notEmpty().withMessage('Login is required'),
     body('password').notEmpty().withMessage('Password is required'),
-    (req: Request, res: Response, next: NextFunction) => {
+    (req: Request, res: Response<APIResponse<unknown>>, next: NextFunction) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
+        return res.status(422).json({ success: false, error: errors.array() });
       }
       next();
     },
@@ -23,33 +24,40 @@ class AuthMiddleware {
 
   static checkAuth = async (
     req: Request,
-    res: Response,
+    res: Response<APIResponse<unknown>>,
     next: NextFunction
   ) => {
     try {
       const authHeader = req.headers.authorization;
 
       if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Token Not Found' });
+        return res
+          .status(401)
+          .json({ success: false, error: 'Token Not Found' });
       }
 
       const token = authHeader.split(' ')[1];
       const decoded = jwt.verify(token, config.authKey);
 
       if (typeof decoded === 'string')
-        return res.status(401).json({ message: 'Invalid Token Format' });
+        return res
+          .status(401)
+          .json({ success: false, error: 'Invalid Token Format' });
 
       const user = await UserModel.findById(decoded.userId);
 
-      if (!user) return res.status(404).json({ message: 'User not found' });
+      if (!user)
+        return res
+          .status(404)
+          .json({ success: false, error: 'User not found' });
 
       (req as VerifiedUserRequest).user = user.id;
       next();
     } catch (error) {
       if (error instanceof jwt.TokenExpiredError) {
-        return res.status(401).json({ message: 'Token Expired' });
+        return res.status(401).json({ success: false, error: 'Token Expired' });
       } else if (error instanceof jwt.JsonWebTokenError) {
-        return res.status(401).json({ message: 'Invalid Token' });
+        return res.status(401).json({ success: false, error: 'Invalid Token' });
       } else {
         next(error);
       }
